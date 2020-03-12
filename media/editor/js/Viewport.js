7fc0f054cc9d86088cb8e774f9d7a3bc739f5b40
Jared Deckard <jared.deckard@gmail.com>
2020-03-12 11:16:03 -0500

    Fix the viewport position

--- media/editor/js/Viewport.js
+++ media/editor/js/Viewport.js
@@ -8,7 +8,7 @@ var Viewport = function ( editor ) {
 
 	var container = new UI.Panel();
 	container.setId( 'viewport' );
-	container.setPosition( 'absolute' );
+	// container.setPosition( 'absolute' );
 
 	container.add( new Viewport.Info( editor ) );
 
